724b680d7cdb2c5a6cf0c42eab81b9df81932cc8
Nexusrex <ahmedtahoun55@gmail.com>
2025-02-01 01:52:21 +0200

    Trail bug: Lack of delta usage

--- Particles/trail.gd
+++ Particles/trail.gd
@@ -1,16 +1,25 @@
 extends Line2D
 
-@export var MAX_POINTS: int = 4
-var point_queue: Array = []
+@export var MAX_POINTS: int = 2
+var _point_queue: Array = []
+var _timer : Timer
 
-func _process(_delta: float) -> void:
-	point_queue.push_front(get_parent().position)
+func _ready() -> void:
+	_timer = Timer.new()
+	_timer.wait_time = 0.05
+	_timer.one_shot = false
+	_timer.connect("timeout", Callable(self, _update_trail.get_method()))
+	add_child(_timer)
+	_timer.start()
+
+func _update_trail() -> void:
+	_point_queue.push_front(get_parent().position)
 	if points.size() > MAX_POINTS:
-		point_queue.pop_back()
+		_point_queue.pop_back()
 	
 	clear_points()
 
-	for point: Vector2 in point_queue:
+	for point: Vector2 in _point_queue:
 		add_point(point)
 
 func stop() -> void:
